731bd6865ffc27673bd5264eacbeda4b3e9f96b6
xkylz1 <fauzansaputra741@gmail.com>
2024-09-30 23:27:54 +0800

    feat: add fauzan path

---
+++ scripts/fauzan.js
@@ -0,0 +1,21 @@
+const fsAsync = require('fs').promises;
+
+async function rewriteAndReadFile(contentFileUtama) {
+    const filePath = "./index.txt";
+    const newContent = 'This text is written by Fauzan\n';
+
+    try {
+        await fsAsync.writeFile(filePath, newContent);
+
+        const updatedContent = await fsAsync.readFile(filePath, 'utf-8');
+
+        const responseContent = `Isi file asli sebelum diubah:\n${contentFileUtama}\n\nIsi file setelah diubah:\n${updatedContent}`;
+
+        return responseContent;
+    } catch (error) {
+        console.error('Error:', error);
+        throw new Error('Error occurred while processing Melinda\'s request.');
+    }
+}
+
+module.exports = rewriteAndReadFile;
